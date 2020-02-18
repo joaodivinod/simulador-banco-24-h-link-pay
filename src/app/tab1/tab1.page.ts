5338cbf927dcc1d1610e2269ead9647c67c6584b
Gabriel Lucas <g.lucas.santos@gmail.com>
2020-02-18 12:04:09 -0300

    Update tab1.page.ts
    add(*) chamada da pagina de qrcode

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private nav: NavController) {}
+  
+  
+  callQrcode() {
+    this.nav.navigateForward('/qrcode');
+  }
 
 }
